6e1e0baec54fa255b8b4135ebdf6de770d695355
mmdreza zeydani <mmdzyzfrontendz2002@gmail.com>
2025-01-15 18:18:06 +0330

    expanding the structure of my blog app,finished the routes api route for crud,added prisma file in lib folder

---
+++ src/app/api/blogs/route.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../../../../prisma/lib/prisma";
+import { NextResponse } from "next/server";
+export async function GET() {
+    const blogs = await prisma.blog.findMany();
+    return NextResponse.json(blogs);
+  }
+  export async function POST(req: Request) {
+    const { title, description, imageUrl } = await req.json();
+    const newBlog = await prisma.blog.create({
+      data: { title, description, imageUrl },
+    });
+    return NextResponse.json(newBlog);
+  }
+  export async function DELETE(req: Request) {
+    const { id } = await req.json();
+    try {
+        await prisma.blog.delete({ where: { id } });
+        return NextResponse.json({ success: true });
+      } catch (error) {
+        return NextResponse.json({ success: false, error: "Blog not found" }, { status: 404 });
+      }
+      
+  }
+  
